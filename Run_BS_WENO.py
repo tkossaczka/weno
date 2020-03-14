946858ef023c2f41b30b1a8297307b341e960011
Tatiana Kossaczka <tkossaczka@gmail.com>
2020-03-14 20:26:07 +0100

    restructured, but not working yet.

--- Run_BS_WENO.py
+++ Run_BS_WENO.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from BS_WENO import BS_WENO
 from compute_omegas5 import compute_omegas5
 from compute_omegas6 import compute_omegas6
-from initial_condition import initial_condition
 import torch
 from torch import nn, optim # Sets of preset layers and optimizers
 import torch.nn.functional as F # Sets of functions such as ReLU
@@ -11,51 +10,60 @@ from torchvision import datasets, transforms # Popular datasets, architectures a
 
 
 class WENONetwork(nn.Module):
-    def __init__(self, pre_omegas5, pre_omegas6):
+    def __init__(self):
         super().__init__()
-        self.omegas5 = nn.Parameter(pre_omegas5, requires_grad=True)
-        self.omegas6 = nn.Parameter(pre_omegas6, requires_grad=True)
+        params = self.get_params()
+        u=self.initial_condition()
+        self.pre_omegas6=compute_omegas6(u,params['m'],1,params['e'])
+        self.pre_omegas5=compute_omegas5(u,params['m'],1,params['e'])
+        self.omegas5 = nn.Parameter(self.pre_omegas5, requires_grad=True)
+        self.omegas6 = nn.Parameter(self.pre_omegas6, requires_grad=True)
+
+    def get_params(self):
+        params = dict()
+        params["sigma"] = 0.3;
+        params["rate"] = 0.1;
+        params["E"] = 50;
+        params["T"] = 1;
+        params["e"] = 10 ** (-13);
+        params["xl"] = -6
+        params["xr"] = 1.5
+        params["m"] = 80;
+        return params
+
+    def initial_condition(self):
+        params = self.get_params()
+        E=params['E']
+        m=params['m']
+        xl=params['xl']
+        xr=params['xr']
+        Smin = np.exp(xl) * E;
+        Smax = np.exp(xr) * E;
+        G = np.log(Smin / E);
+        L = np.log(Smax / E);
+        x = np.linspace(G, L, m + 1)
+        u = torch.zeros((x.shape[0]))[:, None]
+        for k in range(0, m + 1):
+            if x[k] > 0:
+                u[k, 0] = 1 / E;
+            else:
+                u[k, 0] = 0;
+        return u
 
     def forward(self):
-        sigma = 0.3;
-        rate = 0.1;
-        E = 50;
-        T = 1;
-        e = 10 ** (-13);
-        xl = -6
-        xr = 1.5
-        m = 80;
-        omegas5=self.omegas5
-        omegas6=self.omegas6
-        V, S, tt = BS_WENO(sigma, rate, E, T, e, xl, xr, m, omegas5, omegas6)
+        params = self.get_params()
+        V, S, tt = BS_WENO(params["sigma"], params["rate"], params["E"], params["T"], params["e"], params["xl"],
+                           params["xr"], params["m"], self.omegas5, self.omegas6)
         return V
 
     def return_S_tt(self):
-        sigma = 0.3;
-        rate = 0.1;
-        E = 50;
-        T = 1;
-        e = 10 ** (-13);
-        xl = -6
-        xr = 1.5
-        m = 80;
-        omegas5 = self.omegas5
-        omegas6 = self.omegas6
-        V, S, tt = BS_WENO(sigma, rate, E, T, e, xl, xr, m, omegas5, omegas6)
+        params = self.get_params()
+        V, S, tt = BS_WENO(params["sigma"], params["rate"], params["E"], params["T"], params["e"], params["xl"],
+                           params["xr"], params["m"], self.omegas5, self.omegas6)
         return S, tt
 
 
-m = 80;
-e = 10 ** (-13);
-E = 50;
-xl = -6
-xr = 1.5
-u=initial_condition(E,xl,xr,m)
-
-pre_omegas6=compute_omegas6(u,m,1,e,None)
-pre_omegas5=compute_omegas5(u,m,1,e,None)
-
-train_model=WENONetwork(pre_omegas5, pre_omegas6)
+train_model=WENONetwork()
 
 V=train_model.forward()
 
@@ -68,19 +76,20 @@ optimizer = optim.SGD(train_model.parameters(), lr=0.001)
 
 S,tt=train_model.return_S_tt()
 plt.plot(S, V.detach().numpy())
+plt.show()
 V
 
-for k in range(10):
-    V_train = train_model.forward()
-
-    # Train model:
-    optimizer.zero_grad()  # Clear gradients
-    # loss = criterion(V_train, V_zeroflux) # Calculate loss
-    difference=torch.min(V_train[1:-1]-V_train[0:-2],0)
-    loss = my_loss(difference)
-    loss.backward()  # Backward pass
-    optimizer.step()  # Optimize weights
-    print(k, loss, train_model.omegas5, train_model.omegas6)
-
-S,tt=train_model.return_S_tt()
-plt.plot(S, V_train.detach().numpy())
+# for k in range(10):
+#     V_train = train_model.forward()
+#
+#     # Train model:
+#     optimizer.zero_grad()  # Clear gradients
+#     # loss = criterion(V_train, V_zeroflux) # Calculate loss
+#     difference=torch.min(V_train[1:-1]-V_train[0:-2],0)
+#     loss = my_loss(difference)
+#     loss.backward()  # Backward pass
+#     optimizer.step()  # Optimize weights
+#     print(k, loss, train_model.omegas5, train_model.omegas6)
+#
+# S,tt=train_model.return_S_tt()
+# plt.plot(S, V_train.detach().numpy())
